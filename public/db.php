f34403eb4d8a28b1eec19cf7f266417aa8bec41f
Exadra37 <exadra37@gmail.com>
2017-03-23 22:42:52 +0000

    First docker compose draft with nginx, php 7.1 and mysql.

---
+++ public/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$connection = new PDO("mysql:host=mysql;dbname=docker_laravel;charset=utf8", 'docker_laravel', 'docker_secret');
+
+$query = $connection->query("SHOW DATABASES");
+
+$databases = $query->fetchAll(PDO::FETCH_COLUMN);
+
+echo "<h4>mysql> SHOW DATABASES;</h4>";
+
+foreach ($databases as $database) {
+
+    echo "<p>{$database}</p>";
+}
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'db.php';
+
+phpinfo();
