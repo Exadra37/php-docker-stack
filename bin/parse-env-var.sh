796da7751c6a263da3add6bb08b29de1ff588dac
Exadra37 <exadra37@gmail.com>
2017-10-10 08:30:48 +0100

    Improved option --setup-docker-stack.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- bin/parse-env-var.sh
+++ bin/parse-env-var.sh
@@ -4,32 +4,76 @@ set -e
 
 function Parse_Env_Var_From_Env_File()
 {
-    local env_var="${1?}"
+    local _env_var="${1?}"
 
-    local env_file="${2?}"
+    local _env_file="${2?}"
 
     # TODO: only grep lines that are not commented out.
-    local env_var_line="$(grep -i "${env_var}=" "${env_file}")"
+    local _env_var_line="$(grep -i "${_env_var}=" "${_env_file}")"
 
     # PHP_IMAGE=exadra37/php7-fpm:latest -> exadra37/php7-fpm:latest
-    local docker_image="${env_var_line##*=}"
+    local _env_var_default_value="${_env_var_line##*=}"
 
-    echo "${docker_image}"
+    echo "${_env_var_default_value}"
 }
 
 function Parse_Env_Var_From_Docker_Compose()
 {
-    local env_var="${1?}"
+    local _env_var="${1?}"
 
-    local docker_compose_file="${2?}"
+    local _docker_compose_file="${2?}"
 
-    local env_var_line="$(grep -i "${env_var}:-" "${docker_compose_file}")"
+    local _env_var_line="$(grep -i "${_env_var}:-" "${_docker_compose_file}")"
 
     # image: ${PHP_IMAGE:-exadra37/php7-fpm:latest} -> exadra37/php7-fpm:latest
-    local docker_image="${env_var_line##*:-}"
+    local _env_var_default_value="${_env_var_line##*:-}"
 
     # removes } from end of string
-    local docker_image="${docker_image::-1}"
+    local _env_var_default_value="${_env_var_default_value::-1}"
 
-    echo "${docker_image}"
+    echo "${_env_var_default_value}"
+}
+
+function Parse_Left_Env_Var_Value_From_Env_File()
+{
+    local _env_var="${1?}"
+
+    local _env_file="${2?}"
+
+    local _env_var_value="$(Parse_Env_Var_From_Env_File "${_env_var}" "${_env_file}")"
+
+    echo "${_env_var_value%:*}"
+}
+
+function Parse_Left_Env_Var_Value_From_Docker_Compose()
+{
+    local _env_var="${1?}"
+
+    local _docker_compose_file="${2?}"
+
+    local _env_var_value="$(Parse_Env_Var_From_Docker_Compose "${_env_var}" "${_docker_compose_file}")"
+
+    echo "${_env_var_value%:*}"
+}
+
+function Parse_Right_Env_Var_Value_From_Env_File()
+{
+    local _env_var="${1?}"
+
+    local _env_file="${2?}"
+
+    local _env_var_value="$(Parse_Env_Var_From_Env_File "${_env_var}" "${_env_file}")"
+
+    echo "${_env_var_value#*:}"
+}
+
+function Parse_Right_Env_Var_Value_From_Docker_Compose()
+{
+    local _env_var="${1?}"
+
+    local _docker_compose_file="${2?}"
+
+    local _env_var_value="$(Parse_Env_Var_From_Docker_Compose "${_env_var}" "${_docker_compose_file}")"
+
+    echo "${_env_var_value#*:}"
 }
